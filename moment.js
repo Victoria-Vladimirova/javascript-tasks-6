856cd78c7c3d5dfbdc255ceb6f1a8ba911b6c428
Victoria-Vladimirova <wiktorija.wladimirowa@gmail.com>
2015-11-10 01:06:17 +0500

    Update moment.js

--- moment.js
+++ moment.js
@@ -1,20 +1,44 @@
 'use strict';
 
+var MINUTES_IN_HOUR = 60;
+var MINUTES_IN_DAY = 1440;
+
 module.exports = function () {
     return {
         // Здесь как-то хранится дата ;)
-        date: null,
+        weekTime: null,
 
         // А здесь часовой пояс
-        timezone: null,
+        timezone: -new Date().getTimezoneOffset() / MINUTES_IN_HOUR,
+
+        toStringWithLeadingZero: function (num) {
+            num += '';
+            if (num.length === 1) {
+                num = '0' + num;
+            }
+            return num;
+        },
 
         // Выводит дату в переданном формате
         format: function (pattern) {
+            var timeOffset = this.weekTime + this.timezone * MINUTES_IN_HOUR;
+            var daysOfWeek = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+            var day = daysOfWeek[Math.floor(timeOffset / MINUTES_IN_DAY)];
+            var hours = Math.floor((timeOffset % MINUTES_IN_DAY) / MINUTES_IN_HOUR);
+            var minutes = timeOffset % MINUTES_IN_HOUR;
+
+            return pattern.replace('%DD', day).replace('%HH', this.toStringWithLeadingZero(hours))
+                .replace('%MM', this.toStringWithLeadingZero(minutes));
         },
 
         // Возвращает кол-во времени между текущей датой и переданной `moment`
         // в человекопонятном виде
         fromMoment: function (moment) {
+        },
+
+        set date(value) {
+            this.weekTime = value;
         }
+
     };
 };
