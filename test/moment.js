e37a6af410b2c50bff26045121dead0ede1e4155
Victoria-Vladimirova <wiktorija.wladimirowa@gmail.com>
2015-11-10 01:12:07 +0500

    Update test/moment.js

--- test/moment.js
+++ test/moment.js
@@ -6,7 +6,7 @@ var robbery = require('../robbery');
 describe('Базовое тестирование объекта moment', function () {
     it('format()', function () {
         var gang = require('fs').readFileSync('gang.json', 'utf-8');
-        
+
         var robberyMoment = robbery.getAppropriateMoment(gang, 90, {
             from: '09:00+5',
             to: '21:00+5'
@@ -20,4 +20,54 @@ describe('Базовое тестирование объекта moment', functi
             'Ограбление должно состоятся в ВТ. Всем быть готовыми к 16:00!'
         );
     });
+
+    it('format()', function () {
+        var gang = require('fs').readFileSync('gang.json', 'utf-8');
+
+        var robberyMoment = robbery.getAppropriateMoment(gang, 60, {
+            from: '23:00+5',
+            to: '00:00+5'
+        });
+
+        var actual = robberyMoment.format(
+            'Ограбление должно состоятся в %DD. Всем быть готовыми к %HH:%MM!'
+        );
+
+        expect(actual).to.be.equal(
+            'Ограбление должно состоятся в СР. Всем быть готовыми к 23:00!'
+        );
+    });
+    it('format()', function () {
+        var gang = require('fs').readFileSync('gang.json', 'utf-8');
+
+        var robberyMoment = robbery.getAppropriateMoment(gang, 600, {
+            from: '09:00+5',
+            to: '21:00+5'
+        });
+
+        var actual = robberyMoment.format(
+            'Ограбление должно состоятся в %DD. Всем быть готовыми к %HH:%MM!'
+        );
+
+        expect(actual).to.be.equal(
+            'Ограбление должно состоятся в ЧТ. Всем быть готовыми к 09:00!'
+        );
+    });
+    it('format()', function () {
+        var gang = require('fs').readFileSync('gang.json', 'utf-8');
+
+        var robberyMoment = robbery.getAppropriateMoment(gang, 90, {
+            from: '09:00-5',
+            to: '11:00-5'
+        });
+
+        var actual = robberyMoment.format(
+            'Ограбление должно состоятся в %DD. Всем быть готовыми к %HH:%MM!'
+        );
+
+        expect(actual).to.be.equal(
+            'Ограбление должно состоятся в СР. Всем быть готовыми к 19:00!'
+        );
+    });
+
 });
